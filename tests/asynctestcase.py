abd49f97700af76e416911b5fbf8f3943ab3f2b9
Bill Fisher <william.w.fisher@gmail.com>
2016-11-28 16:43:34 -0700

    Use separate AsyncTestCase class.

---
+++ tests/asynctestcase.py
@@ -0,0 +1,41 @@
+""" Implements AsyncTestCase class.
+"""
+
+import asyncio
+import inspect
+import unittest
+
+# Test timeout in seconds
+_TIMEOUT = 10.0
+
+# Method decorator for async tests.
+def _wrap_async(func):
+    """ Method decorator for async tests. """
+    def _wrap(self):
+        task = asyncio.wait_for(func(self), _TIMEOUT)
+        AsyncTestCase.loop.run_until_complete(task)
+    return _wrap
+
+
+class AsyncTestCase(unittest.TestCase):
+    """ TestCase subclass that supports `async def` tests.
+
+    Coroutine functions are wrapped in a function that runs them using
+    `AsyncTestCase.loop.run_until_complete`.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        AsyncTestCase.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(cls.loop)
+        # Wrap test_ functions and setUp/tearDown if they are coroutine
+        # functions.
+        for key in dir(cls):
+            if key.startswith('test_') or key in ('setUp', 'tearDown'):
+                func = getattr(cls, key)
+                if inspect.iscoroutinefunction(func):
+                    setattr(cls, key, _wrap_async(func))
+
+    @classmethod
+    def tearDownClass(cls):
+        AsyncTestCase.loop.close()
--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,13 +1,11 @@
-import unittest
 import asyncio
 import time
+from unittest import skipIf
+from .asynctestcase import AsyncTestCase
 from pylibofp.connection import Connection
 
 # Max message size is 1MB.
 MSG_LIMIT = 2**20
-
-# Test timeout in seconds
-TIMEOUT = 8.0
 
 # Skip tests that send big messages.
 SKIP_BIG_MESSAGES = True
@@ -16,38 +14,18 @@ SKIP_BIG_MESSAGES = True
 LIBOFP_ARGS = [] #['--trace=rpc,msg', '--loglevel=debug']
 
 
-# Method decorator for async tests.
-def run_async(func):
-    def async_test_wrapper(self):
-        task = asyncio.wait_for(func(self), TIMEOUT)
-        ConnectionTestCase.loop.run_until_complete(task)
-    return async_test_wrapper
+class ConnectionTestCase(AsyncTestCase):
 
-
-class ConnectionTestCase(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(cls.loop)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.loop.close()
-
-    @run_async
     async def setUp(self):
         self.conn = Connection(libofp_args=LIBOFP_ARGS)
         await self.conn.connect()
 
-    @run_async
     async def tearDown(self):
         self.conn.close()
         await self.conn.disconnect()
 
     # Basic tests.
 
-    @run_async
     async def test_rpc(self):
         # If we write a valid JSON-RPC method, we should get a result back.
         msg = b'{"id":1234,"method":"OFP.DESCRIPTION"}\n'
@@ -55,7 +33,6 @@ class ConnectionTestCase(unittest.TestCase):
         result = await self.conn.readline()
         self.assertPrefix(result, b'{"id":1234,"result":{"major_version":0,"minor_version":1,"software_version":')
 
-    @run_async
     async def test_rpc_no_id(self):
         # If we write a valid JSON-RPC method with no id, there should be no response.
         msg = b'{"method":"OFP.DESCRIPTION"}\n'
@@ -64,7 +41,6 @@ class ConnectionTestCase(unittest.TestCase):
             result = await asyncio.wait_for(self.conn.readline(), 1.0)
             self.assertIsNone(result)
 
-    @run_async
     async def test_rpc_send_no_datapath(self):
         # If we write a valid JSON-RPC OFP.SEND method, there should be a response.
         msg = b'{"id":7,"method":"OFP.SEND","params":{"version":4,"type":"FEATURES_REQUEST"}}\n'
@@ -72,14 +48,12 @@ class ConnectionTestCase(unittest.TestCase):
         result = await self.conn.readline()
         self.assertPrefix(result, b'{"id":7,"error":{"code":-65000,"message":"YAML:1:39: error: unable to locate connection; no datapath_id or conn_id')
 
-    @run_async
     async def test_rpc_send_no_id_datapath(self):
         msg = b'{"method":"OFP.SEND","params":{"version":4,"type":"FEATURES_REQUEST","xid":12}}\n'
         self.conn.write(msg)
         result = await self.conn.readline()
         self.assertPrefix(result, b'{"method":"OFP.ALERT","params":{"conn_id":0,"datapath_id":"","xid":12,"time":')
 
-    @run_async
     async def test_rpc_send_missing_datapath(self):
         # If we write a valid JSON-RPC OFP.SEND method, there should be a response.
         msg = b'{"id":8,"method":"OFP.SEND","params":{"datapath_id":"00:00:00:00:00:00:00:01","type":"FEATURES_REQUEST"}}\n'
@@ -87,7 +61,6 @@ class ConnectionTestCase(unittest.TestCase):
         result = await self.conn.readline()
         self.assertPrefix(result, b'{"id":8,"error":{"code":-65000,"message":"YAML:1:39: error: unable to locate datapath_id 00:00:00:00:00:00:00:01')
 
-    @run_async
     async def test_rpc_hex_id(self):
         # If we write a valid JSON-RPC method with a hexadecimal ID, we should 
         # get a result back.
@@ -98,7 +71,6 @@ class ConnectionTestCase(unittest.TestCase):
 
     # Test the connection transport limits.
 
-    @run_async
     async def test_short_malformed(self):
         # Write a short malformed json message.
         self.conn.write(b'hello\n')
@@ -106,8 +78,7 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertEqual(result, b'{"id":null,"error":{"code":-32600,"message":"YAML:1:1: error: not a mapping\\nhello\\n^~~~~"}}\n')
         await self._still_alive()
 
-    @unittest.skipIf(SKIP_BIG_MESSAGES, "skipping big message tests")
-    @run_async
+    @skipIf(SKIP_BIG_MESSAGES, "skipping big message tests")
     async def test_write_max_size(self):
         # Write a maximum sized message.
         junk = b'x' * (MSG_LIMIT - 1) + b'\n'
@@ -116,8 +87,7 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertPrefix(result, b'{"id":null,"error":{"code":-32600,"message":"YAML:1:1: error: not a mapping\\nxxx')
         await self._still_alive()
 
-    @unittest.skipIf(SKIP_BIG_MESSAGES, "skipping big message tests")
-    @run_async
+    @skipIf(SKIP_BIG_MESSAGES, "skipping big message tests")
     async def test_write_too_big(self):
         # Write a message that is too big.
         junk = b'x' * MSG_LIMIT + b'\n'
@@ -128,9 +98,8 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertEqual(result, b'{"id":null,"error":{"code":-32600,"message":"RPC request is too big"}}\n')
         await self._end_of_input()
 
-    @unittest.skipIf(SKIP_BIG_MESSAGES, "skipping big message tests")
-    @run_async
-    async def test_massive_write(self):
+    @skipIf(SKIP_BIG_MESSAGES, "skipping big message tests")
+    async def test_zmassive_write(self):
         # Now, write enough messages to really back things up.
         count = 5
         junk = b'z' * (MSG_LIMIT - 1) + b'\n'
@@ -151,7 +120,6 @@ class ConnectionTestCase(unittest.TestCase):
 
     # Test invalid input.
 
-    @run_async
     async def test_rpc_invalid_json(self):
         # If we write an invalid message, we should get back an error with a 
         # id=null.
@@ -161,7 +129,6 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertPrefix(result, b'{"id":null,"error":{"code":-32600,"message":')
         await self._still_alive()
 
-    @run_async
     async def test_rpc_incorrect_jsonrpc_version(self):
         # If we write a message with an incorrect jsonrpc version, we should
         # get back an error.
@@ -171,7 +138,6 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertPrefix(result, b'{"id":13,"error":{"code":-32600,"message":"YAML:1:2: error: Unsupported jsonrpc version')
         await self._still_alive()
 
-    @run_async
     async def test_rpc_invalid_id(self):
         msg = b'{"id":"abc","method":"OFP.DESCRIPTION"}\n'
         self.conn.write(msg)
@@ -179,7 +145,6 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertPrefix(result, b'{"id":null,"error":{"code":-32600,"message":"YAML:1:7: error: Invalid RPC ID')
         await self._still_alive()
 
-    @run_async
     async def test_rpc_id_null(self):
         # If we write a message with a null id, it is treated as a valid id.
         msg = b'{"id":null,"method":"OFP.DESCRIPTION"}\n'
@@ -188,7 +153,6 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertPrefix(result, b'{"id":null,"result":{"major_version":')
         await self._still_alive()
 
-    @run_async
     async def test_rpc_id_too_big(self):
         for id_val in [2**64-2, 2**64-1, 2**64, 2**64+1]:
             msg = b'{"id":%d,"method":"OFP.DESCRIPTION"}\n' % id_val
@@ -197,7 +161,6 @@ class ConnectionTestCase(unittest.TestCase):
             self.assertPrefix(result, b'{"id":null,"error":{"code":-32600,"message":"YAML:1:7: error: Invalid RPC ID')
             await self._still_alive()
 
-    @run_async
     async def test_rpc_id_max(self):
         id_val = 2**64-3
         msg = b'{"id":%d,"method":"OFP.DESCRIPTION"}\n' % id_val
@@ -206,7 +169,6 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertPrefix(result, b'{"id":%d,"result":{"major_version":' % id_val)
         await self._still_alive()
 
-    @run_async
     async def test_ofp_send_invalid(self):
         # If we write an invalid OFP.SEND message, we should get back an error.
         msg = b'{"id":321,"method":"OFP.SEND","params":{"type":"foo"}}\n'
@@ -215,7 +177,6 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertPrefix(result, b'{"id":321,"error":{"code":-32600,"message":"YAML:1:48: error: unknown value \\"foo\\" Did you mean \\"HELLO\\"?')
         await self._still_alive()
 
-    @run_async
     async def test_ofp_send_invalid_no_id(self):
         # If we write an invalid OFP.SEND message with no id, we should get back
         # a OFP.ALERT notification.
@@ -227,7 +188,6 @@ class ConnectionTestCase(unittest.TestCase):
 
     # The following tests are taken from http://www.jsonrpc.org/specification
     
-    @run_async
     async def test_rpc_call_of_nonexistant_method(self):
         msg = b'{"jsonrpc": "2.0", "method": "foobar", "id": "1"}\n'
         self.conn.write(msg)
@@ -235,7 +195,6 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertPrefix(result, b'{"id":1,"error":{"code":-32601,"message":"YAML:1:30: error: unknown method')
         await self._still_alive()
 
-    @run_async
     async def test_rpc_call_with_invalid_json(self):
         msg = b'{"jsonrpc": "2.0", "method": "foobar, "params": "bar", "baz]\n'
         self.conn.write(msg)
@@ -243,7 +202,6 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertPrefix(result, b'{"id":null,"error":{"code":-32600,"message":')
         await self._still_alive()
 
-    @run_async
     async def test_rpc_call_with_invalid_request_object(self):
         msg = b'{"jsonrpc": "2.0", "method": 1, "params": "bar"}\n'
         self.conn.write(msg)
@@ -251,7 +209,6 @@ class ConnectionTestCase(unittest.TestCase):
         self.assertPrefix(result, b'{"id":null,"error":{"code":-32600,"message":')
         await self._still_alive()
 
-    @run_async
     async def test_rpc_call_with_invalid_batch(self):
         msg = b'[1,2,3]\n'
         self.conn.write(msg)
