f334f4723b3d30661c8802521ce218999501728f
Bill Fisher <william.w.fisher@gmail.com>
2017-03-19 18:17:54 -0700

    Tweaks to PktDecode.

--- pylibofp/pktdecode.py
+++ pylibofp/pktdecode.py
@@ -1,13 +1,15 @@
-from .objectview import ObjectView
+from .objectview import ObjectView, item_iter
 
 
 class PktDecode(ObjectView):
-    """Concrete class that represents a PktDecode.
-    """
+    """Concrete class that represents a decoded packet."""
 
     def __init__(self, fields):
-        assert isinstance(fields, (list, tuple))
         super().__init__({})
+
+        if not isinstance(fields, (list, tuple)):
+            raise ValueError('Expected list or tuple')
+        
         for field in fields:
             key = field['field'].lower()
             assert key != 'payload'
@@ -15,4 +17,13 @@ class PktDecode(ObjectView):
 
     @staticmethod
     def to_list(obj):
-        return [dict(field=k.upper(), value=v) for k, v in obj.items() if k != 'payload']
+        """Convert decoded packet to a list of fields."""
+        return [_make_field(k, v) for k, v in item_iter(obj) if k != 'payload']
+
+
+def _make_field(name, value):
+    if isinstance(value, tuple):
+        if len(value) != 2:
+            raise ValueError('len(tuple) != 2')
+        return dict(field=name.upper(), value=value[0], mask=value[1])
+    return dict(field=name.upper(), value=value)
--- tests/test_pktdecode.py
+++ tests/test_pktdecode.py
@@ -4,7 +4,7 @@ from pylibofp.pktdecode import PktDecode
 
 class PktDecodeTestCase(unittest.TestCase):
 
-    def test_init(self):
+    def test_basic(self):
         data = [
             dict(field='A', value='a'),
             dict(field='B', value=2)
@@ -13,6 +13,20 @@ class PktDecodeTestCase(unittest.TestCase):
         self.assertEqual(pkt.a, 'a')
         self.assertEqual(pkt.b, 2)
 
-    def test_invalid_init(self):
-        pkt = PktDecode(dict(A='a'))
+        fields = PktDecode.to_list(pkt)
+        fields.sort(key=_by_field)
+        self.assertEqual(fields, data)
 
+    def test_invalid_init(self):
+        with self.assertRaises(ValueError):
+            pkt = PktDecode(dict(A='a'))
+
+    def test_to_list(self):
+        data = dict(a = 'a', b = 2, c='ddd')
+        fields = PktDecode.to_list(data)
+        fields.sort(key=_by_field)
+        self.assertEqual(fields, [dict(field='A', value='a'), dict(field='B', value=2), dict(field='C', value='ddd')])
+
+
+def _by_field(item):
+    return item['field']
