30d21c7f0d1eb7bbac840e4d2cde4f9e05503735
Bill Fisher <william.w.fisher@gmail.com>
2017-07-18 19:37:45 -0700

    Add test for self-signed certificates.

--- test/integration_tests.sh
+++ test/integration_tests.sh
@@ -17,29 +17,44 @@ $CMD -m ofp_app.demo.layer2 --unknown-argument || true
 echo "Test layer2 demo with simulator (count=50)"
 $CMD -m ofp_app.demo.simulator \
         --listen-endpoints=6653 \
-        --exit-timeout=3 \
-        --simulator-count=50 \
+        --sim-timeout=3 \
+        --sim-count=50 \
         --loglevel=warning \
-        ofp_app.demo.layer2
+        --x-modules=ofp_app.demo.layer2
 
 echo "Test conntest demo with simulator (count=10)"
 $CMD -m ofp_app.demo.simulator \
         --listen-endpoints=6653 \
-        --exit-timeout=3 \
-        ofp_app.demo.conntest
+        --sim-timeout=3 \
+        --x-modules=ofp_app.demo.conntest
 
 echo "Test layer2 demo with simulator using TLS"
 $CMD -m ofp_app.demo.simulator \
         --listen-endpoints=6653 \
-        --cert="$SCRIPT_DIR/ctl-cert.pem" \
-        --privkey="$SCRIPT_DIR/ctl-privkey.pem" \
-        --cacert="$SCRIPT_DIR/sw-cacert.pem" \
-        --exit-timeout=3 \
-        --simulator-count=5 \
+        --listen-cert="$SCRIPT_DIR/ctl-cert.pem" \
+        --listen-privkey="$SCRIPT_DIR/ctl-privkey.pem" \
+        --listen-cacert="$SCRIPT_DIR/sw-cacert.pem" \
+        --sim-timeout=3 \
+        --sim-count=5 \
         --loglevel=info \
         --sim-cert="$SCRIPT_DIR/sw-cert.pem" \
         --sim-privkey="$SCRIPT_DIR/sw-privkey.pem" \
         --sim-cacert="$SCRIPT_DIR/ctl-cacert.pem" \
-        ofp_app.demo.layer2
+        --x-modules=ofp_app.demo.layer2
+
+echo "Test layer2 demo with simulator using TLS (self-signed certs)"
+# This test also tells the simulator to use multiple switch certificates.
+$CMD -m ofp_app.demo.simulator \
+        --listen-endpoints=6653 \
+        --listen-cert="$SCRIPT_DIR/ss-cntl.cert" \
+        --listen-privkey="$SCRIPT_DIR/ss-cntl.key" \
+        --listen-cacert="$SCRIPT_DIR/ss-sw.cacert" \
+        --sim-timeout=3 \
+        --sim-count=5 \
+        --loglevel=info \
+        --sim-cert="$SCRIPT_DIR/ss-sw1.cert" \
+        --sim-privkey="$SCRIPT_DIR/ss-sw1.key" \
+        --sim-cacert="$SCRIPT_DIR/ss-cntl.cert" \
+        --x-modules=ofp_app.demo.layer2 
 
 exit 0
