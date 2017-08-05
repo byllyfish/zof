1a20eb7d1a97ed377581519b8d54135b83b0017f
Bill Fisher <william.w.fisher@gmail.com>
2017-08-04 21:32:15 -0700

    Clean up.

--- setup.py
+++ setup.py
@@ -4,35 +4,35 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+import os
 import re
+from setuptools import setup, find_packages
 
-here = path.abspath(path.dirname(__file__))
-description_path = path.join(here, 'README.rst')
-version_path = path.join(here, 'zof', '__init__.py')
 
-# Read long_description.rst.
-with open(description_path, encoding='utf-8') as f:
-    long_description = f.read()
+HERE = os.path.abspath(os.path.dirname(__file__))
+README_PATH = os.path.join(HERE, 'README.rst')
+VERSION_PATH = os.path.join(HERE, 'zof', '__init__.py')
 
-# Extract version number.
-with open(version_path, encoding='utf-8') as f:
-    version_regex = re.compile(r"(?m)__version__\s*=\s*'(\d+\.\d+\.\d+)'")
-    version = version_regex.search(f.read()).group(1)
+
+def _get_description(path):
+    with open(path, encoding='utf-8') as afile:
+        return afile.read()
+
+
+def _get_version(path):
+    with open(path, encoding='utf-8') as afile:
+        regex = re.compile(r"(?m)__version__\s*=\s*'(\d+\.\d+\.\d+)'")
+        return regex.search(afile.read()).group(1)
 
 
 setup(
     name='zof',
     packages=find_packages(exclude=['test']),
-    version=version,
+    version=_get_version(VERSION_PATH),
     license='MIT',
 
     description='OpenFlow App Framework',
-    long_description=long_description,
+    long_description=_get_description(README_PATH),
     keywords='openflow controller',
 
     # The project's main homepage and author.
