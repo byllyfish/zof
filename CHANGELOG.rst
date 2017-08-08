f0980706a5500b50e19593d3a15c98dda475805c
Bill Fisher <william.w.fisher@gmail.com>
2017-08-08 13:51:55 -0700

    Bump version.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,12 @@
 CHANGELOG
 =========
 
+Version 0.1.1
+-------------
+
+- Fix bug in datapath service.
+
+
 Version 0.1.0
 -------------
 
--- zof/__init__.py
+++ zof/__init__.py
@@ -12,4 +12,4 @@ from .api_args import common_args  # noqa: E402,F401
 from .api_compile import compile  # noqa: E402,F401
 from .api_functions import get_datapaths, post_event, ensure_future, connect, close, get_connections, add_identity
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
