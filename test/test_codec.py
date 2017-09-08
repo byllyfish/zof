e110a5f148bc9463404f2455afb51b23072e9162
Bill Fisher <william.w.fisher@gmail.com>
2017-09-07 18:57:56 -0700

    Rename api_encode to codec module.

--- test/test_codec.py
+++ test/test_codec.py
@@ -1,37 +1,38 @@
 import unittest
 import zof
 from zof.objectview import make_objectview
+from zof.codec import encode
 
 
-class EncodeTestCase(unittest.TestCase):
+class CodecTestCase(unittest.TestCase):
 
     def test_empty(self):
         with self.assertRaisesRegex(ValueError, r'YAML:1:1: error: not a document'):
-            zof.encode('')
+            encode('')
 
         with self.assertRaisesRegex(ValueError, r'YAML:1:1: error: not a document'):
-            zof.encode('---\n\n...\n')
+            encode('---\n\n...\n')
 
     def test_hello(self):
-        result = zof.encode('type: HELLO\nversion: 1')
+        result = encode('type: HELLO\nversion: 1')
         self.assertEqual(result, b'\x01\x00\x00\x08\x00\x00\x00\x00')
 
-        result = zof.encode('type: HELLO')
+        result = encode('type: HELLO')
         self.assertEqual(result, b'\x04\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x08\x00\x00\x00\x10')
 
-        result = zof.encode('type: HELLO', version=0)
+        result = encode('type: HELLO', version=0)
         self.assertEqual(result, b'\x06\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x08\x00\x00\x00~')
 
     def test_hello_error(self):
         with self.assertRaisesRegex(ValueError, r"YAML:1:1: error: missing required key 'type'"):
-            zof.encode('typ: HELLO')
+            encode('typ: HELLO')
 
         with self.assertRaisesRegex(ValueError, r'YAML:1:7: error: unknown value "HELO" Did you mean "HELLO"?'):
-            zof.encode('type: HELO')
+            encode('type: HELO')
 
     def test_hello_object(self):
-        result = zof.encode({'type': 'HELLO', 'version': 4})
+        result = encode({'type': 'HELLO', 'version': 4})
         self.assertEqual(result, b'\x04\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x08\x00\x00\x00\x10')
 
-        result = zof.encode(make_objectview({'type': 'HELLO', 'version': 4}))
+        result = encode(make_objectview({'type': 'HELLO', 'version': 4}))
         self.assertEqual(result, b'\x04\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x08\x00\x00\x00\x10')
