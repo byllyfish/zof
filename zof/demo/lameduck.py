82c94c54612c14f9228deeab49a4a133b68bcaab
Bill Fisher <william.w.fisher@gmail.com>
2017-09-25 14:32:57 -0700

    Add lameduck demo app (for cbench).

---
+++ zof/demo/lameduck.py
@@ -0,0 +1,24 @@
+import zof
+
+
+APP = zof.Application('lameduck')
+
+
+FLOW_MOD = zof.compile('''
+type: FLOW_MOD
+msg:
+  table_id: 0
+  command: ADD
+  match:
+   - field: ETH_DST
+     value: $eth_dst
+''')
+
+
+@APP.message('packet_in')
+def packet_in(event):
+    FLOW_MOD.send(eth_dst=event.msg.pkt.eth_dst)
+
+
+if __name__ == '__main__':
+    zof.run()
