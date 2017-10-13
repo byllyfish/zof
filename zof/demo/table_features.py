a3c24f8cd19405c4c8c2071a375ab8a7b082354e
Bill Fisher <william.w.fisher@gmail.com>
2017-10-12 18:27:23 -0700

    Update table_features demo.

--- zof/demo/table_features.py
+++ zof/demo/table_features.py
@@ -3,33 +3,63 @@ import zof
 
 APP = zof.Application('table_features', exception_fatal=True)
 
+DESC = zof.compile('''
+type: REQUEST.DESC
+''')
+
 TABLE_FEATURES = zof.compile('''
 type: REQUEST.TABLE_FEATURES
 msg: []
 ''')
 
 
-#@APP.event('start')
-#async def start(event):
-#    conn_id = await zof.connect('192.168.0.4:9600')
-#    tfm = await TABLE_FEATURES.request(conn_id=conn_id)
-#    #APP.logger.info(tfm)
-#    zof.post_event({'event': 'EXIT'})
+async def table_features_match_fields1():
+    """Get set of all fields available in OF matches.
+    """
+    fields = set()
+    tfm_count = 0
+    entry_count = 0
+
+    async for tfm in TABLE_FEATURES.request():
+        tfm_count += 1
+        for tfm_entry in tfm['msg']:
+            entry_count += 1
+            for field in tfm_entry['match']:
+                fields.add(field)
+
+    print('Processed %d entries in %d TableFeatures messages' % (entry_count, tfm_count))
+    return fields
+
+
+async def table_features_match_fields2():
+    """Get set of all fields available in OF matches.
+    """
+    fields = set()
+    tfm_count = 0
+    entry_count = 0
+
+    tfm = await TABLE_FEATURES.request()
+    tfm_count += 1
+    for tfm_entry in tfm['msg']:
+        entry_count += 1
+        for field in tfm_entry['match']:
+            fields.add(field)
+
+    print('Processed %d entries in %d TableFeatures messages' % (entry_count, tfm_count))
+    return fields
 
 
 @APP.message('channel_up')
 async def channel_up(event):
-    fields = set()
-    count = 0
-    async for tfm in TABLE_FEATURES.request():
-        count += 1
-        for tfm_entry in tfm['msg']:
-            for field in tfm_entry['match']:
-                fields.add(field)
+    info = await DESC.request()
+    print('Description: %s %s' % (info['msg']['hw_desc'], info['msg']['sw_desc']))
 
-    print('%d messages' % count)
+    fields = await table_features_match_fields1()
     for field in sorted(list(fields)):
         print(field)
+
+    fields2 = await table_features_match_fields2()
+    assert fields2 == fields
 
     zof.post_event({'event': 'EXIT'})
 
