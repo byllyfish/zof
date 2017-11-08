c3aa9ecd26b1bd65e97820b25554f910cdf36dfd
Bill Fisher <william.w.fisher@gmail.com>
2017-11-08 13:46:01 -0700

    Remove Event class and make_event function.

--- zof/event.py
+++ zof/event.py
@@ -1,25 +1,4 @@
-from .objectview import ObjectView, to_json, from_json
-from .pktview import pktview_from_list
-
-
-class Event(ObjectView):
-    """Concrete class that represents an Event."""
-
-    def __init__(self, d):
-        try:
-            # Any value with key 'data' MUST be a binary type.
-            # If there's no `data` key, the rest of this is skipped.
-            data = bytes.fromhex(d['data'])
-            d['data'] = data
-            # If there's no `_pkt` key, the rest is skipped.
-            pkt = pktview_from_list(d.pop('_pkt'))
-            d['pkt'] = pkt
-            # If there's no 'x_pkt_pos' key in pkt, the rest is skipped.
-            pkt.payload = data[pkt['x_pkt_pos']:]
-        except KeyError:
-            pass
-        finally:
-            self.__dict__ = d
+from .objectview import to_json, from_json
 
 
 def load_event(event):
@@ -34,16 +13,3 @@ def dump_event(event):
     if isinstance(event, str):
         return event.encode('utf-8')
     return to_json(event).encode('utf-8')
-
-
-def make_event(**kwds):
-    if not kwds:
-        raise ValueError('Missing event arguments')
-    return _make_event(kwds)
-
-
-def _make_event(obj):
-    for key in obj:
-        if isinstance(obj[key], dict):
-            obj[key] = _make_event(obj[key])
-    return Event(obj)
