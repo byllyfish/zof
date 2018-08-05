14fa4db978bec34ea1a905982951cdfefa83b67b
Bill Fisher <william.w.fisher@gmail.com>
2018-08-04 18:44:56 -0700

    Add pycodestyle and pydocstyle.

--- run_travis.sh
+++ run_travis.sh
@@ -4,4 +4,7 @@ set -e
 
 python -m pytest --pylint --pylint-error-types=EF \
                  --mypy \
-                 --cov=zoflite --cov-report=term-missing
+                 --cov=zoflite --cov-report=term-missing --cov-fail-under=90
+
+pycodestyle zoflite
+pydocstyle zoflite
