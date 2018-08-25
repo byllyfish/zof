e6c360fd8fe985b4eb2d190fdaa72a46bfdd2f43
Bill Fisher <william.w.fisher@gmail.com>
2018-08-25 00:27:07 -0700

    Log value associated with missing attribute.

--- zof/pktview.py
+++ zof/pktview.py
@@ -17,7 +17,7 @@ def pktview_alias(name, converter=(lambda x: x)):
             return converter(self.__dict__[name])
         except KeyError:
             raise AttributeError(
-                'PktView object has no attribute "%s"' % name) from None
+                'PktView object has no attribute "%s": %r' % (name, self)) from None
 
     def _fset(self, value):
         self.__dict__[name] = value
