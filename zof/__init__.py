f5263f27ed872cce3b1e05fe7197086bb8e94435
Bill Fisher <william.w.fisher@gmail.com>
2018-09-07 18:36:28 -0700

    check python version

--- zof/__init__.py
+++ zof/__init__.py
@@ -2,9 +2,16 @@
 
 __version__ = '0.99.0'
 
-from zof.controller import Controller
-from zof.configuration import Configuration
-from zof.datapath import Datapath
-from zof.driver import Driver
-from zof.exception import RequestError
-from zof.packet import Packet
+import sys
+if sys.version_info[:2] < (3, 7):
+    import platform
+    raise NotImplementedError(
+        'zof does not support Python %s. Python 3.7 required.'
+        % platform.python_version())
+
+from zof.controller import Controller  # noqa: E402
+from zof.configuration import Configuration  # noqa: E402
+from zof.datapath import Datapath  # noqa: E402
+from zof.driver import Driver  # noqa: E402
+from zof.exception import RequestError  # noqa: E402
+from zof.packet import Packet  # noqa: E402
