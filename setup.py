31eb8cf4b6ddf8cb2b81e3e2c8cb0d43808f4be7
Bill Fisher <william.w.fisher@gmail.com>
2018-10-01 12:06:16 -0700

    Initial packaging.

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+# All options are specified in setup.cfg.
+# https://setuptools.readthedocs.io/en/latest/setuptools.html#configuring-setup-using-setup-cfg-files
+setup()
