419d39aca67a788cb76370f520a878c1a79f6ae8
Bill Fisher <william.w.fisher@gmail.com>
2018-10-04 19:41:34 -0700

    Add comment for mypy.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup  # type: ignore
 
 # All options are specified in setup.cfg.
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#configuring-setup-using-setup-cfg-files
