180f7336d5b10e9601fff650cb00dc9758eceef0
Bill Fisher <william.w.fisher@gmail.com>
2018-10-21 13:15:21 -0700

    Revert "Refactor into version.py"
    This reverts commit 63df2e7d91e6e102eef74e9844629f4e95b316ee.

--- zof/__init__.py
+++ zof/__init__.py
@@ -1,6 +1,15 @@
 """zof framework."""
 
-from zof.version import __version__
+__version__ = '0.50.0'
+
+import sys
+if sys.version_info[:2] < (3, 6):  # pragma: no cover
+    import platform
+    raise NotImplementedError(
+        'zof does not support Python %s. Python 3.6 required.' %
+        platform.python_version())
+
+# pylint: disable=wrong-import-position
 
 from zof.controller import Controller, get_controller  # noqa: E402
 from zof.configuration import Configuration  # noqa: E402
--- zof/version.py
+++
@@ -1,3 +0,0 @@
-"""zof version number."""
-
-__version__ = '0.50.0'
